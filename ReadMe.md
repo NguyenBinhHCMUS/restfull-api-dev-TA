c16d05845c88724820e6c45ba58f69c666f117f2
CTLT-D3410-2\Administrator <ctv3.bcn1.e@ctgroupvietnam.com>
2022-10-01 10:08:49 +0700

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# create
+
+npm init -y
+npm i express mongoose dotenv cors morgan
+npm i -D typescript ts-node-dev
+
+# run
+
+npm run build
+npm start
+npm run dev
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
